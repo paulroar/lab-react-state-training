f5fc811c0252082ceede1269919ea83f31cc75ba
Paulo Prado <paulroar@hotmail.com>
2025-01-17 15:40:37 +0000

    Done

--- src/assets/Components/Dice/Dice.jsx
+++ src/assets/Components/Dice/Dice.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import emptyDice from "../src/assets/images/dice-empty.png";
+import emptyDice from "../src/assets/images/emptyDice.png";
 import dice1 from "../src/assets/images/dice1.png";
 import dice2 from "../src/assets/images/dice2.png";
 import dice3 from "../src/assets/images/dice3.png";
